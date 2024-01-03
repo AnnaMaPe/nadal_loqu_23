6a643b991cfb36dd19861f0fb02f353c437a9bf6
Anna Marrodán <anna.marrodan@gmail.com>
2024-01-03 17:16:23 +0100

    Modify title in Header component

--- src/components/Header/Header.ts
+++ src/components/Header/Header.ts
@@ -4,7 +4,7 @@ export class Header extends Component {
   render() {
     super.render();
 
-    this.element.innerHTML = `<h1 class="${styles["title"]}" >Bon Nadal</h1><img class="${styles["header-image"]}" src="https://michaelmcclennan.com/wp-content/uploads/2021/03/Dancing-Jesus-resize.gif" alt="Jesus dancing" width="100">
+    this.element.innerHTML = `<h1 class="${styles["title"]}">Completa el teu Nadal</h1><img class="${styles["header-image"]}" src="https://michaelmcclennan.com/wp-content/uploads/2021/03/Dancing-Jesus-resize.gif" alt="Jesus dancing" width="100">
 `;
   }
 }
